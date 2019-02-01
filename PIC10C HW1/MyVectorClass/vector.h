6505142e225723870fef16fff559ee79e77580c6
Alex Ngun <ukmerryxd@gmail.com>
2019-02-01 14:11:57 -0800

    Adding definition for the big4

--- PIC10C HW1/MyVectorClass/vector.h
+++ PIC10C HW1/MyVectorClass/vector.h
@@ -28,5 +28,48 @@ private:
   static const int INIT_CAP = 10;
 };
 
+vector::vector()
+:the_data(nullptr), the_size(0), the_capacity(INIT_CAP)
+{
+  the_data = new int[INIT_CAP];
+}
+
+vector::vector( const vector& source )
+: the_data(nullptr), the_size(source.the_size), the_capacity(source.the_capacity)
+{
+  the_data = new int[the_capacity];
+  
+  // Deep copy of internal array
+  for ( int i = 0 ; i < the_size ; ++i )
+  {
+    the_data[i] = source.the_data[i];
+  }
+}
+
+vector& vector::operator=(const vector& rhs)
+{
+  if ( this != &rhs )
+  {
+    // Release old memory and request more
+    delete[] the_data;
+    the_data = new int[rhs.the_capacity];
+    
+    // Shallow copy non-pointers
+    the_size = rhs.the_size;
+    the_capacity = rhs.the_capacity;
+    
+    // Deep copy internal array
+    for ( int i = 0 ; i < the_size ; ++i )
+      the_data[i] = rhs.the_data[i];
+  }
+  
+  return *this;
+}
+
+vector::~vector()
+{
+  delete[] the_data;
+}
+
 
 #endif /* vector_h */
