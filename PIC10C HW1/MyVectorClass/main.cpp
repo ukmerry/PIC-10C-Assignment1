fbbfe3b06c0519072367d7b6478ba8cc6fde4387
Alex Ngun <ukmerryxd@gmail.com>
2019-02-01 17:11:05 -0800

    Adding testing code

--- PIC10C HW1/MyVectorClass/main.cpp
+++ PIC10C HW1/MyVectorClass/main.cpp
@@ -11,5 +11,26 @@
 
 int main()
 {
+  vector v1;
+  v1.push_back(1);
+  v1.push_back(2);
+  v1.push_back(3);
+  
+  vector v2;
+  v2.push_back(4);
+  v2.push_back(5);
+  v2.push_back(6);
+  
+  vector v3;
+  
+  v3 = v1 + v2;
+  std::cout << "v1 + v2 = " << v3 << "\n";
+  std::cout << "5 * v2 = " << 5*v2 << "\n";
+  
+  std::cout << "v1 < v2 = " << (v1 < v2) << "\n";
+  std::cout << "v1 > v2 = " << (v1 > v2) << "\n";
+  std::cout << "v1 == v2 = " << (v1 == v2) << "\n";
+  std::cout << "v1 != v2 = " << (v1 != v2) << "\n";
+  
   return 0;
 }
